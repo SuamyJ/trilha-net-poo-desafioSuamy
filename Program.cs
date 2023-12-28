4f6b1696213dac93e30ace18ec916d1c173a44c5
Suamy <suamyvc@hotmail.com>
2023-12-27 21:26:32 -0300

    Implementações do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia("+55081995588777", "G60", "001970-01-010000-0", 128);
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Whatsapp");
+        nokia.Ligar();
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone iphone = new Iphone("+55081955887788", "15 Pro Max", "001870-01-100000-0", 512);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Telegram");
+        iphone.ReceberLigacao();
+    }
+}
